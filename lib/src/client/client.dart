c67ab605bbd983214292a583270ce3bce4816ed5
Frank <frankborden2@gmail.com>
2018-10-08 23:52:27 -0700

    first graphics with canvas

--- lib/src/client/client.dart
+++ lib/src/client/client.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:html';
 
+import 'package:gremlins/src/data/map_points.dart' show mapPoints;
 import 'package:gremlins/src/model/model.pb.dart';
 import 'package:web_socket_channel/html.dart';
 
@@ -18,8 +19,18 @@ class Client {
   final List<Spot> allSpots = [];
   final List<Card> allCards = [];
   PlayerStateViewData playerStateView;
+  CanvasRenderingContext2D renderer;
+  final ImageElement image = ImageElement(src: 'map.png');
 
   Client() {
+    final CanvasElement canvas = querySelector('#canvas');
+    canvas
+      ..width = 1531
+      ..height = 861
+      ..style.backgroundColor = '0';
+    renderer = canvas.context2D;
+    image.onLoad.first.then((_) => renderer.drawImage(image, 0, 0));
+
     final StreamController<Action> controller = StreamController.broadcast();
     final ButtonElement button = querySelector('#button');
     button.onClick.listen((_) {
@@ -59,6 +70,28 @@ class Client {
         ..addAll(data.initialData.cards);
     } else if (data.hasPlayerStateView()) {
       playerStateView = data.playerStateView;
+
+      renderer
+        ..clearRect(0, 0, 1531, 861)
+        ..drawImage(image, 0, 0);
+      _drawPlayer(playerStateView.self.currentSpotId, '#F00');
+      playerStateView.others.forEach(
+          (playerView) => _drawPlayer(playerView.currentSpotId, '#0F0'));
+
+      querySelector('#resources').text =
+          playerStateView.self.resources.toString();
+      querySelector('#cards').text = playerStateView.self.handCardIds
+          .map((id) => allCards[id].name)
+          .join(',');
+      querySelector('#other-resources').text =
+          playerStateView.others.first.resources.toString();
     }
+  }
+
+  void _drawPlayer(int spotId, String color) {
+    final List<int> drawPoint = mapPoints[spotId];
+    renderer
+      ..fillStyle = color
+      ..fillRect(drawPoint.first, drawPoint.last, 50, 50);
   }
 }
